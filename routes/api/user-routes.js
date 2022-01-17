0be82005a68734612cd1aaf5383d559594115847
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-17 00:04:56 -0500

    login route verifies user password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,29 @@ router.post('/', (req, res) => {
           res.status(500).json(err);
         });
     });
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+          username: req.body.username
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that username!' });
+          return;
+        }
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });          
+      });  
+    
+})
     
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
