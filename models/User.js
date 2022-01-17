77f2db246d8b83a70d81ec0d8a26bf170e359b65
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-16 20:59:46 -0500

    created user model and routes

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+      {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          username: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              // this means the password must be at least four characters long
+              len: [4]
+            }
+          }
+      },
+      {    
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+      }   
+);
+
+module.exports = User;
