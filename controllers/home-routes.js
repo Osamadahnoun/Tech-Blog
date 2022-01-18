0acc33500a4c2caeb49d608ac9495f78d9b0bdc4
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-18 18:03:35 -0500

    signup form functional

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,4 +31,13 @@ router.get('/', (req, res) => {
       });  
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+
 module.exports = router;
