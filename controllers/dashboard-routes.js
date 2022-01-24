bf24981586c1c4faa34910796194bd2d88799793
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-24 18:02:18 -0500

    user can post blog

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  
+
+router.get('/create', (req, res) => {
+    res.render('create-post', { loggedIn: true });
+  });
+
+  router.get('/edit', (req, res) => {
+    res.render('edit-post', { loggedIn: true });
+  });
+  
+
+
+
+module.exports = router;
---
+++ public/javascript/add-post.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('textarea[name="post-content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.create-btn').addEventListener('click', newFormHandler);
