81622bb0d7ead071d6ed41a2f3cba3684a00549d
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-24 16:57:46 -0500

    fixed date formats

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./helpers/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
