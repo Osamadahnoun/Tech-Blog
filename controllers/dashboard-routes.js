a87b3b97398f4c8e48c957304c747fbadd7101b5
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-24 18:33:09 -0500

    user can edit/delete blog posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const withAuth = require('../helpers/auth');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     Post.findAll({
       where: {
         user_id: req.session.user_id
@@ -36,8 +37,51 @@ router.get('/create', (req, res) => {
     res.render('create-post', { loggedIn: true });
   });
 
-  router.get('/edit', (req, res) => {
-    res.render('edit-post', { loggedIn: true });
+  router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: [
+          'id',
+          'title',
+          'content',
+          'created_at',
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+          }
+    
+          // serialize the data
+          const post = dbPostData.get({ plain: true });
+    
+          // pass data to template
+          res.render('edit-post', { 
+            post,
+            loggedIn: true
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
   });
   
 
